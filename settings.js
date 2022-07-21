92095eaa7e7dc15db72391a86ecf6d05a2a3d96e
mystro1 <58634414+mystro1@users.noreply.github.com>
2022-07-21 16:21:20 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,31 +19,30 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is Mystro Youness🔥` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['0707057652'] //Owner number in aive msg
+global.premium = ['0707057652'] //Owner Number info
+global.ownernomer = '0707057652' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = 'Younes' //Owner Name
+global.botname = ' Botmystro' //Bot Name
 
-global.button = '☰ GITHUB ☰' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://github.com/prabhasha2006' //Change Your Button's URL
+global.button = '☰ YT ☰' //Costomize A Button Name In Alive Message
+global.btnurl = 'https://www.youtube.com/channel/UCFktItsqvHj_TtTwNixdQbQ' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = 'Mystro' //Bot Pacage Name
+global.author = 'younes' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '©_Kumuthu.' //In list menu
+global.footer = '©_mSR.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
+global.ig = 'https://www.youtube.com/channel/UCFktItsqvHj_TtTwNixdQbQ' //Github Account!
 
 global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
 	
